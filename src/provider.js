8a2208fadc86a935db2bc8aad9dd0962f2f55abb
amir <returnamor@gmail.com>
2022-04-12 00:22:21 +0430

    chore: update pckage.json - remove react import

--- src/provider.js
+++ src/provider.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { createPortal } from "react-dom";
 import Notifier from "./context";
 import NotifyWrapper from "./notifyWrapper";
--- src/withNotifier.js
+++ src/withNotifier.js
@@ -1,4 +1,3 @@
-import React from "react";
 import Notifier from "./context";
 
 const withNotifier = (WrapperComponent) => {
